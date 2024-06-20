30ee8bc194722b3bfe8c737b365c410da14bc1f2
ahrbilzakaria <zakariaahr@gmail.com>
2024-06-20 11:39:49 +0100

    first 3

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 500,
+      repeat: -1,
+      yoyo: true,
+      duration: 2,
+      ease: "bounce in",
+      stagger: 0.5,
+      rotate: 360,
+    });
+  }, []);
 
   return (
     <main>
